03fa7b141189ad972d005ae394a3c3bcee647c5a
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-02-13 18:23:24 -0300

    wip: disabling normal loging, enabling clave unica login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :autorize_cenabast_user!, unless: :devise_controller?
+
+  private
+
+  def autorize_cenabast_user!
+    return if spree_user_signed_in?
+
+    redirect_to new_spree_user_session_path
+  end
 end
--- app/models/cenabast/spree/user_decorator.rb
+++ app/models/cenabast/spree/user_decorator.rb
@@ -2,6 +2,9 @@ module Cenabast
   module Spree
     module UserDecorator
       def self.prepended(base)
+        base.devise :database_authenticatable, :rememberable,
+                    :trackable # , :omniauthable, omniauth_providers: %i[clave_unica]
+
         base.include Cenabast::Spree::User::StorePreference
       end
 
