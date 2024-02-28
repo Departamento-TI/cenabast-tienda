68bbe3c88075e6ca9ae57292ed367e5bba82d8e3
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-02-28 16:50:25 -0300

    progress in mage ai process of loading product/contract data from cenabast apis

---
+++ mage_ai/cenabast/data_loaders/pull_product_data.py
@@ -0,0 +1,147 @@
+import os
+import io
+import pandas as pd
+import requests
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    # Get logger
+    logger = kwargs.get('logger')
+
+    # Get product sku for call payload
+    product_sku = kwargs.get('product_sku')
+
+    # Get token from API
+    token = get_token(logger)
+    # Get payload to use
+    payload = products_payload(product_sku)
+    # Get general product data
+    product_data = get_product_data(token, payload, logger)
+
+    # Filter unique products by 'codigoProducto' and that codigoProducto is higher than 500016000
+    already_added = set()
+    filtered_products = [
+        item for item in product_data['content']
+        if item.get('codigoProducto')
+        and int(item['codigoProducto']) > 500016000
+        and item.get('codigoProducto') not in already_added
+        and not already_added.add(item.get('codigoProducto'))
+    ][:3]  # Take only the first 3 records
+
+    # Get contract information for each unique product
+    for product_info in filtered_products:
+        codigo_producto = product_info['codigoProducto']
+        contracts = get_contracts_data(token, codigo_producto, logger)
+        product_info['contracts'] = contracts
+
+    return filtered_products
+
+def base_url():
+    return "https://testaplicacionesweb.cenabast.cl:7001"
+
+def login_url():
+    return f"{base_url()}/interoperabilidad/servicios/auth"
+
+def products_url():
+    return f"{base_url()}/interoperabilidad/servicios/materiales/listacatalogo"
+
+def contracts_url(codigo_producto):
+    return f"{base_url()}/interoperabilidad/servicios/materiales/contratos/{codigo_producto}"
+
+def products_payload(product_sku):
+    if product_sku is not None:
+        return {
+            "codigoProducto": product_sku,
+            "tipoProducto": None,
+            "grupoArticulo": None,
+            "codigoSector": None
+        }
+    else:
+        return {
+            "codigoProducto": None,
+            "tipoProducto": 'ZCEN',
+            "grupoArticulo": None,
+            "codigoSector": 'S1'
+        }
+
+def login_params():
+    # Get login params from env
+    return {
+        "user": os.environ['CENABAST_API_USER'],
+        "password": os.environ['CENABAST_API_PASSWORD']
+    }
+
+# Request functions
+
+def get_token(logger=None):
+    log_request_info(logger, "POST", login_url(), login_params())
+    response = requests.post(login_url(), json=login_params())
+
+    if response.status_code == 200:
+        log_response_info(logger, response)
+        return response.json().get("content")
+    else:
+        log_error_info(logger, response)
+        raise Exception(f"Failed to obtain token. Status code: {response.status_code}, Response: {response.text}")
+
+def get_product_data(token, payload, logger=None):
+    url = products_url()
+    log_request_info(logger, "GET", url, payload)
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        log_response_info(logger, response)
+        return response.json()
+    else:
+        log_error_info(logger, response)
+        raise Exception(f"Failed to obtain data. Status code: {response.status_code}, Response: {response.text}")
+
+def get_contracts_data(token, codigo_producto, logger=None):
+    url = contracts_url(codigo_producto)
+    log_request_info(logger, "GET", url)
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        log_response_info(logger, response)
+        return response.json().get("content")
+    else:
+        log_error_info(logger, response)
+        raise Exception(f"Failed to obtain contract info. Status code: {response.status_code}, Response: {response.text}")
+
+def log_request_info(logger, method, url, payload=None):
+    if logger:
+        logger.info(f"Making {method} request to {url} with payload: {payload}")
+    else:
+        module_logger.info(f"Making {method} request to {url} with payload: {payload}")
+
+def log_response_info(logger, response):
+    truncated_content = response.text[:4000]
+    if logger:
+        logger.info(f"Received response with status code {response.status_code} and content: {truncated_content}")
+    else:
+        module_logger.info(f"Received response with status code {response.status_code} and content: {truncated_content}")
+
+def log_error_info(logger, response):
+    truncated_content = response.text[:4000]
+    if logger:
+        logger.error(f"Request failed with status code {response.status_code} and response: {truncated_content}")
+    else:
+        module_logger.error(f"Request failed with status code {response.status_code} and response: {truncated_content}")
---
+++ mage_ai/cenabast/transformers/filter_product_data.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+import calendar
+
+@transformer
+def transform(data, *args, **kwargs):
+    # Get logger
+    logger = kwargs.get('logger')
+
+    # Specify your transformation logic here
+    current_date = datetime.now()
+
+    # Rewrite contracts to only have valid ones
+    for product in data:
+        if 'contracts' in product:
+            valid_contracts = filter_valid_contracts(product['contracts'], current_date, logger)
+            product['contracts'] = valid_contracts
+
+    # Filter products with valid contract_info
+    valid_products = [
+        product for product in data
+        if len(product['contracts']) > 0
+    ]
+
+    logger.info(f'Amount of total products (before filter): {len(data)}')
+    logger.info(f'Amount of valid products (after filter): {len(valid_products)}')
+
+    return valid_products
+
+def filter_valid_contracts(contracts, current_date, logger):
+    """
+    Filter out invalid contracts based on start and end dates.
+
+    Args:
+        contracts: Array containing information about contracts
+        current_date: The current date
+        logger: logger object
+
+    Returns:
+        List of valid contracts
+    """
+    valid_contracts = [
+        contract for contract in contracts
+        if is_valid_contract(contract, current_date, logger)
+    ]
+    return valid_contracts
+
+def is_valid_contract(contract, current_date, logger):
+    """
+    Check if the contract is valid based on start and end dates.
+
+    Args:
+        contract: Information about the contract
+        current_date: The current date
+        logger: logger object
+
+    Returns:
+        True if the contract is valid, False otherwise
+    """
+    start_date = datetime.strptime(contract.get('fechaInicialContrato'), '%Y%m')
+    end_date = datetime.strptime(contract.get('fechaFinalContrato'), '%Y%m')
+    end_date = end_date + timedelta(days=calendar.monthrange(end_date.year, end_date.month)[1]-1, hours=23, minutes=59, seconds=59)
+
+    logger.debug(f'start_date is: {start_date}')
+    logger.debug(f'current_date is: {current_date}')
+    logger.debug(f'end_date is: {end_date}')
+    
+    logger.debug(f'condition check is: {start_date <= current_date < end_date}')
+
+    return start_date <= current_date < end_date
