9b695279c8b84416314de8ace6097a567c776e02
Felipe Fabio <felipe.fabio@acidlabs.com>
2024-03-28 03:54:08 -0300

    PE188-22 Fix context call class

--- app/models/cenabast/spree/order_receiver_ability.rb
+++ app/models/cenabast/spree/order_receiver_ability.rb
@@ -4,7 +4,7 @@ module Cenabast
       include CanCan::Ability
 
       def initialize(user)
-        can :update, Spree::Order do |order, token|
+        can :update, ::Spree::Order do |order, token|
           !order.completed? && (order.receiver == user.current_receiver || (order.token && token == order.token))
         end
       end
