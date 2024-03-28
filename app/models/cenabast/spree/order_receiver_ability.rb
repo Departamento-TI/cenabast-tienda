59184002e29081077298f0f198687bb0984d5c7e
Felipe Fabio <felipe.fabio@acidlabs.com>
2024-03-28 11:29:32 -0300

    Add ability to current_receiver can show order

--- app/models/cenabast/spree/order_receiver_ability.rb
+++ app/models/cenabast/spree/order_receiver_ability.rb
@@ -4,6 +4,9 @@ module Cenabast
       include CanCan::Ability
 
       def initialize(user)
+        can :show, ::Spree::Order do |order, token|
+          order.receiver == user.current_receiver || (order.token && token == order.token)
+        end
         can :update, ::Spree::Order do |order, token|
           !order.completed? && (order.receiver == user.current_receiver || (order.token && token == order.token))
         end
