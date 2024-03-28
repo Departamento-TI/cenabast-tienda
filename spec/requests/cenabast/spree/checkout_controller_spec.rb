cc2aa3480c5580a8b28de30381be720b0cfd18d9
Felipe Fabio <felipe.fabio@acidlabs.com>
2024-03-28 03:05:17 -0300

    PE188-22 Add request test to test checkout before_address callback

--- spec/requests/cenabast/spree/checkout_controller_spec.rb
+++ spec/requests/cenabast/spree/checkout_controller_spec.rb
@@ -1,32 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Spree::CheckoutController', type: :request do
-  describe 'GET #edit' do
+  describe 'GET #edit before_address callback' do
     let(:receiver) { create(:receiver) }
     let(:user) { create(:user, run: '88.888.888-8', receivers: [receiver]) }
-    let(:order) { create(:order, state: :address) }
+    let(:order) { create(:order_with_line_items, state: :cart, receiver:) }
 
     before do
-      allow_any_instance_of(Spree::CheckoutController).to receive(:set_cache_header)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:set_current_order)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:set_state_if_present).and_return(nil)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:ensure_order_not_completed).and_return(nil)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:ensure_checkout_allowed).and_return(nil)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:ensure_sufficient_stock_lines).and_return(nil)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:ensure_valid_state).and_return(nil)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:associate_user)
-      allow_any_instance_of(Spree::CheckoutController).to receive(:check_authorization).and_return(nil)
-
       allow_any_instance_of(Spree::CheckoutController).to receive(:current_order).and_return(order)
       act_as_logged_in(user)
     end
 
     context 'when order meets minimum purchase per vendor' do
-      xit 'renders the address page' do
+      it 'renders the address page' do
         allow_any_instance_of(Cenabast::Spree::Order::FindLineItemStatsGroupedByVendor).to receive(:call).
           and_return(stats_per_vendor_double(valid_minimum_amount: true))
 
-        get spree.checkout_state_path(order.state)
+        get spree.checkout_path
+        follow_redirect!
 
         expect(order.state).to eq('address')
         expect(response).to have_http_status(:ok)
@@ -34,11 +25,12 @@ RSpec.describe 'Spree::CheckoutController', type: :request do
     end
 
     context 'when order does not meet minimum purchase per vendor' do
-      xit 'redirects to the cart page with an alert' do
-        allow(Cenabast::Spree::Order::FindLineItemStatsGroupedByVendor).to receive(:call).
+      it 'redirects to the cart page with an alert' do
+        allow_any_instance_of(Cenabast::Spree::Order::FindLineItemStatsGroupedByVendor).to receive(:call).
           and_return(stats_per_vendor_double(valid_minimum_amount: false))
 
-        get spree.checkout_state_path(order.state)
+        get spree.checkout_path
+        follow_redirect!
 
         expect(order.state).to eq('cart')
         expect(response).to redirect_to(spree.cart_path)
