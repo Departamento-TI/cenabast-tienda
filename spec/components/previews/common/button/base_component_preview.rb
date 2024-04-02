c60fc1f569fc835fa6582a4adc7cfb8bdeb1e3f8
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-04-02 18:35:05 -0300

    add frontend validation, feedback post button click on checkout address form

--- spec/components/previews/common/button/base_component_preview.rb
+++ spec/components/previews/common/button/base_component_preview.rb
@@ -15,4 +15,22 @@ class Common::Button::BaseComponentPreview < ViewComponent::Preview
 
     render Common::Button::BaseComponent.new(button_params:)
   end
+
+  # @param text text
+  # @param size select { choices: [sm, md, lg, xl, xxl] }
+  # @param hierarchy select { choices: [primary, secondary_color, secondary_gray, tertiary_color, tertiary_gray, link_color, link_gray] }
+  # @param destructive toggle
+  # @param additional_styles text
+  def loading(text: 'Click me', size: 'md', hierarchy: 'primary', destructive: false, additional_styles: '')
+    button_params = {
+      text:,
+      size:,
+      hierarchy:,
+      destructive:,
+      loading: true,
+      additional_styles:
+    }
+
+    render Common::Button::BaseComponent.new(button_params:)
+  end
 end
