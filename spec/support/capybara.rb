b2682d55471873354d7f5b46f874ed74c874a3fd
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-04-08 21:39:14 -0400

    added config for selenium js enabled tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,11 +1,46 @@
+require 'rspec/rails'
+require 'capybara/rspec'
+
+Capybara.register_driver :remote_selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--remote-debugging-pipe')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: 'http://selenium:4444',
+    capabilities: options
+  )
+end
+
+Capybara::Screenshot.register_driver(:remote_selenium) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
+
+Capybara.server_host = '0.0.0.0'
+Capybara.server = :puma
+Capybara.javascript_driver = :remote_selenium
+Capybara.app_host = "http://#{Socket.gethostname}:#{Capybara.server_port}"
+
 RSpec.configure do |config|
   config.include Capybara::RSpecMatchers, type: :component
 
   config.before(:each, type: :system) do |example|
-    if example.metadata[:js]
-      driven_by :selenium_chrome_headless
-    else
-      driven_by :rack_test
-    end
+    # `Capybara.app_host` is reset in the RSpec before_setup callback defined
+    # in `ActionDispatch::SystemTesting::TestHelpers::SetupAndTeardown`, which
+    # is annoying as hell, but not easy to "fix". Just set it manually every
+    # test run.
+    Capybara.app_host = "http://#{Socket.gethostname}:#{Capybara.server_port}"
+
+    driver = if example.metadata[:js]
+               :remote_selenium
+             else
+               :rack_test
+             end
+
+    driven_by driver
   end
 end
--- spec/system/product_page_spec.rb
+++ spec/system/product_page_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Visiting Product Page', type: :system do
-  let(:user) { create(:user) }
+  let(:receiver) { create(:receiver) }
+  let(:user) { create(:user, receivers: [receiver]) }
   let(:product) { create(:cenabast_product) }
 
-  before do
+  before(:each) do
+    create_cenabast_stores
+    store = Spree::Store.first
+    store.update(default: true)
+    act_with_current_store(store)
+
     puts product.stores.pluck(:name)
     act_as_logged_in(user)
+    user.toggle_receiver(receiver)
+    user.toggle_store(store)
+
     visit "/products/#{product.slug}"
   end
 
-  xit 'shows product' do
-    expect(page).to have_text(Spree.t('spree.pdp.about_product'))
-  end
-
-  xit 'should display product`s ATC code' do
+  it 'should display product`s ATC code', js: true do
     expect(page).to have_text(product.generic_product.code_atc)
   end
 end
