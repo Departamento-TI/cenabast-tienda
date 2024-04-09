1ca63811784d5f2feac811c506a801cf4a939875
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-04-09 15:40:20 -0400

    add selenium dependancy to github rspec action

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -10,10 +10,13 @@ Capybara.register_driver :remote_selenium do |app|
   options.add_argument('--window-size=1920,1080')
   options.add_argument('--force-device-scale-factor=0.95')
 
+  selenium_host = ENV.fetch('SELENIUM_HOST', 'selenium')
+  selenium_port = ENV.fetch('SELENIUM_PORT', '4444')
+
   Capybara::Selenium::Driver.new(
     app,
     browser: :remote,
-    url: 'http://selenium:4444',
+    url: "http://#{selenium_host}:#{selenium_port}",
     capabilities: options
   )
 end
@@ -24,9 +27,12 @@ end
 
 Capybara.default_max_wait_time = 5
 Capybara.server_host = '0.0.0.0'
+Capybara.server_port = ENV.fetch('SERVER_PORT', '9000')
 Capybara.server = :puma
 Capybara.javascript_driver = :remote_selenium
 Capybara.app_host = "http://#{Socket.gethostname}:#{Capybara.server_port}"
+
+puts "Capybara.app_host: #{Capybara.app_host}"
 
 RSpec.configure do |config|
   config.include Capybara::RSpecMatchers, type: :component
