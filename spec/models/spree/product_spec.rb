5709d08a26625697224b465bf087fec0e1754a24
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-04-22 18:34:55 -0400

    re add searchkick to products, remove from variants

--- spec/models/spree/product_spec.rb
+++ spec/models/spree/product_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Spree::Product, type: :model do
     it { should belong_to(:generic_product).class_name('Cenabast::Spree::GenericProduct').optional }
     it { should belong_to(:vendor).class_name('Spree::Vendor').optional }
   end
+
+  describe 'Searchkick', search: true do
+    let!(:store) { create(:store) }
+
+    it 'searches' do
+      create(:product, sku: 'Testing', product:)
+      described_class.search_index.refresh
+      assert_equal ['Testing'], described_class.search('testing').map(&:sku)
+    end
+  end
 end
