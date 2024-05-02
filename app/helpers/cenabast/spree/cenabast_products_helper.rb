a97772043b75b477af4d371682b88f9b4438351f
Leonardo Santis <leonardo.santis.a@gmail.com>
2024-05-02 09:50:18 -0400

    change mercado_publico_link_for_product to only generate url if mercado publico id exists for product

--- app/helpers/cenabast/spree/cenabast_products_helper.rb
+++ app/helpers/cenabast/spree/cenabast_products_helper.rb
@@ -2,10 +2,14 @@ module Cenabast
   module Spree
     module CenabastProductsHelper
       def mercado_publico_link_for_product(product)
-        mercado_publico_id = product&.contract&.mercado_publico_id || '--'
+        mercado_publico_id = product&.contract&.mercado_publico_id
 
-        result_url = "https://www.mercadopublico.cl/Procurement/Modules/RFB/DetailsAcquisition.aspx?idlicitacion=#{mercado_publico_id}"
-        link_to(mercado_publico_id, result_url, { class: 'underline underline-offset-1' })
+        if mercado_publico_id
+          result_url = "https://www.mercadopublico.cl/Procurement/Modules/RFB/DetailsAcquisition.aspx?idlicitacion=#{mercado_publico_id}"
+          link_to(mercado_publico_id, result_url, { class: 'underline underline-offset-1' })
+        else
+          '--'
+        end
       end
     end
   end
